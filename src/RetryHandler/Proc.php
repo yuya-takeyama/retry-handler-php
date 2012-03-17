c0a072ce68a33bc23ccb2a6ead517dd00dceb5d4
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-03-17 15:33:19 +0900

    Add wait option.

--- src/RetryHandler/Proc.php
+++ src/RetryHandler/Proc.php
@@ -5,6 +5,10 @@ use \Exception;
 
 class Proc
 {
+    const DEFAULT_MAX_RETRY = 3;
+    const DEFAULT_WAIT_TIME = 1;
+    const DEFAULT_ACCEPTED_EXCEPTION = 'RetryHandler\RetryException';
+
     /**
      * @var callable
      */
@@ -22,7 +26,23 @@ class Proc
         $this->_proc = $proc;
     }
 
-    public function retry($max)
+    public function retry($options, $moreOptions = array())
+    {
+        if (is_int($options)) {
+            $options = array('max' => $options);
+        }
+        $options = array_merge($options, $moreOptions);
+
+        $max = isset($options['max']) ? $options['max'] : self::DEFAULT_MAX_RETRY;
+        $wait = isset($options['wait']) ? $options['wait'] : self::DEFAULT_WAIT_TIME;
+        $exception = isset($options['accepted_exception']) ?
+            $options['accepted_exception'] :
+            self::DEFAULT_ACCEPTED_EXCEPTION;
+
+        return $this->_retry($max, $wait, $exception);
+    }
+
+    protected function _retry($max, $wait, $acceptedException)
     {
         for ($i = 0; $i < $max; $i++) {
             try {
